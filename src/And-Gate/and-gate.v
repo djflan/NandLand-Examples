d043ddc2f516b3039d759351df90d8dbd8e1e615
Dan Flanigan <djflan@gmail.com>
2025-04-20 12:05:49 -0500

    revised to include output specifier as a wire

--- src/And-Gate/and-gate.v
+++ src/And-Gate/and-gate.v
@@ -7,7 +7,8 @@ module example_and_gate (
     
     input input_1;
     input input_2;
-    output and_result;
+    
+    output wire and_result;
 
     assign and_result = input_1 & input_2;
 endmodule
